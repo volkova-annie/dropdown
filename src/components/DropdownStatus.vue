f3432cddf9f585c14dfb8314b96df4420aeb6911
Volkova Annie <annie_v@bk.ru>
2017-08-23 18:35:27 +0300

    update component dropdown

--- src/components/DropdownStatus.vue
+++ src/components/DropdownStatus.vue
@@ -1,14 +1,21 @@
 <template>
-  <div>
-    <ul id='dropdown-menu'>
-      <span>Претензии</span>
-      <li class='dropdown-menu-option' v-on:click='addOption' v-for='option in options' v-bind:value='[option.id]'><input  class='dropdown' type="checkbox" name=""/>{{option.title}}</li>
+  <div class='container'>
+    <button type="button" name="button" v-on:click='toggleClass' v-bind:class='[isActive ? "arrow" : "", "dropdown-menu-title"]'>
+      <!-- <span class="placeholder">Статус претензии</span> -->
+      <span class="placeholder">{{ placeholder }}</span>
+    </button>
+    <ul id='dropdown-menu' v-bind:class='{"open": isActive}'>
+      <li class='dropdown-menu-option' v-for='option in options'>
+        <label>
+          <input class='dropdown' type='checkbox' v-bind:value='option.value' v-model='option.checked' v-on:click='selectOption(option)'/>{{option.title}}
+        </label>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
-const claim = []
+
 
 export default {
   data()  {
@@ -16,78 +23,159 @@ export default {
       options: [
         {
           title: 'Все',
-          id: 1
+          id: 1,
+          value: '',
+          checked: false
         }, {
           title: 'На проверке',
-          id: 2
+          id: 2,
+          value: 'saved',
+          checked: false
         }, {
           title: 'На рассмотрении',
-          id: 3
+          id: 3,
+          value: 'processed',
+          checked: false
         }, {
           title: 'На доработке',
-          id: 4
+          id: 4,
+          value: 'returned',
+          checked: false
         }, {
           title: 'Согласована',
-          id: 5
+          id: 5,
+          value: 'accepted',
+          checked: false
         }, {
           title: 'Не удовлетворена',
-          id: 6
+          id: 6,
+          value: 'declined',
+          checked: false
         }
-      ]
+      ],
+      isActive: false,
+      selectedOptions: []
+    }
+  },
+  computed: {
+    placeholder() {
+      var arr = this.selectedOptions;
+      var text = this.selectedOptions.map(el => el.title).join(', ');
+      // console.log(text);
+      // console.log(arr);
+      if (arr.length === 4) {
+        // console.log('4 из 5');
+      }
+      if (arr.length === 6) {
+        // console.log('all');
+      }
+      arr.map(function(el) {
+        if (el.id === 1){
+          // console.log('hello');
+        }
+      }
+    );
+      // if ()
+      // return this.selectedOptions.map(el => el.title).join(', ');
     }
   },
   methods: {
-    addOption: function() {
-      console.log(this.value);
-      claim.push({
-        title: this.options
-      })
-      // console.log(claim);
+    selectOption: function(option) {
+      if (option.isChecked) {
+        this.selectedOptions = this.selectedOptions.filter(el => el.id !== option.id);
+      } else {
+        option.isChecked = true;
+        this.selectedOptions.push(option);
+        console.log(option.checked);
+      }
+    },
+    toggleClass: function() {
+      this.isActive = !this.isActive
+      console.log(this.options.checked);
     }
   }
-
-//
-
-//   data() {
-//     return {
-//       data
-//     }
-//   },
-//   methods: {
-//
-//     }
 }
+
 
 </script>
 
 <style>
-
-  ul#dropdown-menu {
-    display: block;
-    border: 1px solid black;
-    width: 150px;
-    padding: 1px 5px;
+  .container {
+    position: absolut;
+    width: 200px;
+    font-family: Roboto;
+    font-size: 16px;
+  }
+  .dropdown-menu-title {
+    position: absolute;
+    display: flex;
+    justify-content: space-around;
+    position: inherit;
+    width: 202px;
+    color: black;
+    margin: 0;
+    padding: 0;
+    font-family: Roboto;
+    font-size: 17px;
+    height: 22px;
+    font-weight: bold;
+    border: 1px solid #000;
+    text-align: left;
+    background-color: inherit;
+    cursor: pointer;
+  }
+  .dropdown-menu-title:after {
+    position: absolute;
+    left: 185px;
+    top: 12px;
+    content: "▼";
+    font-size: 14px;
+  }
+  .dropdown-menu-title:focus {
+    outline: none;
+  }
+  .arrow:after {
+    position: absolute;
+    left: 185px;
+    top: 12px;
+    content: "▲";
+    font-size: 14px;
+  }
+  #dropdown-menu {
+    display: none;
+    width: 200px;
+    padding: 0;
+    margin: 0;
     list-style: none;
     color: gray;
+    border-left: 1px solid #000;
+    border-right: 1px solid #000;
+  }
+  #dropdown-menu.open {
+    display: block;
+  }
+  .open:last-child {
+    border-bottom: 1px solid #000;
+  }
+  ul#dropdown-menu.open:not(:last-child) {
   }
   li.dropdown-menu-option {
-    display: none;
-    border-bottom: 1px solid gray;
-    width: calc(100% + 10px);
-    margin-left: -5px;
-  }
-  li.dropdown-menu-option:first-child {
-    border-top: 1px solid gray;
-  }
-  li.dropdown-menu-option:last-child {
-    border-bottom: none;
-  }
-  ul#dropdown-menu:hover > .dropdown-menu-option {
-    display: block;
+    border-bottom: 1px solid #d6d6d6;
+    color: gray;
   }
   li.dropdown-menu-option:hover {
     color: black;
   }
+  li.dropdown-menu-option:last-child {
+    border-bottom: none;
+  }
+  .placeholder {
+    width: 85%;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
 
 
 </style>
